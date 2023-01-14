d7f174c01308bf284862a08b39c214dc01be8a81
Alejandro <aledrums@gmail.com>
2023-01-14 02:27:24 -0500

    parametrized env version and num interations

--- lecture1/main.py
+++ lecture1/main.py
@@ -1,13 +1,17 @@
-import gym
+import sys
 import gym_environments
+import gym
 from agent import TwoArmedBandit
 
-env = gym.make('TwoArmedBandit-v0')
+num_iterations = 100 if len(sys.argv) < 2 else int(sys.argv[1])
+version = "v0" if len(sys.argv) < 3 else sys.argv[2]
+
+env = gym.make(f"TwoArmedBandit-{version}")
 agent = TwoArmedBandit(0.1) 
 
-env.reset()
+env.reset(options={'delay': 1})
 
-for iteration in range(10):
+for iteration in range(num_iterations):
     action = agent.get_action("random")    
     _, reward, _, _, _ = env.step(action)
     agent.update(action, reward) 
