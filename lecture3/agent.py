18dcd7efb3040105d1deb4dd2dd2a4a54eeb5f34
Jesús Pérez <jesuspangulo@ula.ve>
2023-01-28 11:18:00 -0400

    autopep8

--- lecture3/agent.py
+++ lecture3/agent.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class MonteCarlo():
     def __init__(self, states_n, actions_n, gamma, epsilon):
         self.states_n = states_n
@@ -11,7 +12,7 @@ class MonteCarlo():
     def reset(self):
         self.episode = []
         self.q = np.zeros((self.states_n, self.actions_n))
-        self.pi = np.full((self.states_n, self.actions_n), 1/self.actions_n)
+        self.pi = np.full((self.states_n, self.actions_n), 1 / self.actions_n)
         self.returns = np.zeros((self.states_n, self.actions_n))
         self.returns_n = np.zeros((self.states_n, self.actions_n))
 
@@ -21,33 +22,39 @@ class MonteCarlo():
             self._update_q()
             self._update_pi()
             self.episode = []
-            
+
     def _update_q(self):
         states_actions = []
-        [states_actions.append((state, action)) for state,action,_ in self.episode if (state, action) not in states_actions]
+        [states_actions.append((state, action)) for state, action, _ in self.episode if (
+            state, action) not in states_actions]
         for state, action in states_actions:
-            first_occurence = next(i for i,step in enumerate(self.episode) if step[0] == state and step[1] == action)
-            G = sum([step[2]*(self.gamma**i) for i,step in enumerate(self.episode[first_occurence:])])  
+            first_occurence = next(i for i, step in enumerate(
+                self.episode) if step[0] == state and step[1] == action)
+            G = sum([step[2] * (self.gamma**i)
+                    for i, step in enumerate(self.episode[first_occurence:])])
             self.returns[state][action] += G
             self.returns_n[state][action] += 1
-            self.q[state][action] = self.returns[state][action] / self.returns_n[state][action]
-    
+            self.q[state][action] = self.returns[state][action] / \
+                self.returns_n[state][action]
+
     def _update_pi(self):
         states = []
-        [states.append(state) for state,_,_ in self.episode if state not in states]
+        [states.append(state)
+         for state, _, _ in self.episode if state not in states]
         for state in states:
             best_action = np.argmax(self.q[state])
             for action in range(self.actions_n):
                 if action == best_action:
-                    self.pi[state][action] = 1 - self.epsilon + (self.epsilon / self.actions_n)
+                    self.pi[state][action] = 1 - self.epsilon + \
+                        (self.epsilon / self.actions_n)
                 else:
                     self.pi[state][action] = (self.epsilon / self.actions_n)
-    
+
     def get_action(self, state):
-        return np.random.choice(self.actions_n, p = self.pi[state])
+        return np.random.choice(self.actions_n, p=self.pi[state])
 
     def get_best_action(self, state):
         return np.argmax(self.q[state])
-     
+
     def render(self):
         print(f"Values: {self.q}\nPolicy: {self.pi}")
--- lecture3/main.py
+++ lecture3/main.py
@@ -3,17 +3,20 @@ import time
 import gym_environments
 from agent import MonteCarlo
 
-def train(env, agent, episodes):   
+
+def train(env, agent, episodes):
     for _ in range(episodes):
         observation, _ = env.reset()
         terminated, truncated = False, False
         while not (terminated or truncated):
             action = agent.get_action(observation)
-            new_observation, reward, terminated, truncated, _ = env.step(action)
+            new_observation, reward, terminated, truncated, _ = env.step(
+                action)
             agent.update(observation, action, reward, terminated)
             observation = new_observation
 
-def play(env, agent):    
+
+def play(env, agent):
     observation, _ = env.reset()
     terminated, truncated = False, False
     while not (terminated or truncated):
@@ -22,9 +25,14 @@ def play(env, agent):
         env.render()
         time.sleep(1)
 
+
 if __name__ == "__main__":
     env = gym.make('RobotMaze-v0', render_mode="human")
-    agent = MonteCarlo(env.observation_space.n, env.action_space.n, gamma=0.9, epsilon=0.9)
+    agent = MonteCarlo(
+        env.observation_space.n,
+        env.action_space.n,
+        gamma=0.9,
+        epsilon=0.9)
 
     train(env, agent, episodes=100)
     agent.render()
