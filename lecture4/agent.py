898eb8d56e3df6b68806dede3aed7882894d597e
Jesús Pérez <jesuspangulo@ula.ve>
2023-02-04 11:35:13 -0400

    Add lecture 4

---
+++ lecture4/agent.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+class QLearning():
+    def __init__(self, states_n, actions_n, alpha, gamma, epsilon):
+        self.states_n = states_n
+        self.actions_n = actions_n
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.reset()
+
+    def reset(self):
+        self.episode = 0
+        self.iteration = 0
+        self.state = 0
+        self.action = 0
+        self.next_state = 0
+        self.reward = 0        
+        self.q_table = np.zeros((self.states_n, self.actions_n))
+
+    def update(self, current_state, action, next_state, reward, terminated):
+        self._update(current_state, action, next_state, reward, terminated)
+        self.q_table[current_state, action] = self.q_table[current_state, action] + self.alpha * (reward + self.gamma * np.max(self.q_table[next_state])-self.q_table[current_state, action])
+
+    def _update(self, current_state, action, next_state, reward, terminated):        
+        self.iteration += 1
+        self.state = current_state
+        self.action = action
+        self.next_state = next_state
+        self.reward = reward
+        if terminated:
+            self.episode += 1
+            self.iteration = 0
+
+    def get_action(self, state, mode):
+        if mode == 'random':
+            return np.random.choice(self.actions_n)
+        elif mode == 'greedy':
+            return np.argmax(self.q_table[state])
+        elif mode == 'epsilon-greedy':
+            rdm = np.random.uniform(0, 1)
+            if rdm < self.epsilon:
+                return np.random.choice(self.actions_n)
+            else:
+                return np.argmax(self.q_table[state])
+
+    def render(self, mode='values'):
+        if (mode == 'step'):
+            print("Episode: {}, Iteration: {}, State: {}, Action: {}, Next state: {}, Reward: {}".format(
+            self.episode, self.iteration, self.state, self.action, self.next_state, self.reward))
+        elif (mode == 'values'):
+            print("Q-Table: {}".format(self.q_table))
---
+++ lecture4/main.py
@@ -0,0 +1,37 @@
+import gym
+import gym_environments
+from agent import QLearning
+
+#RobotBattery-v0, Taxi-v3, FrozenLake-v1, RobotMaze-v0
+ENVIRONMENT = 'RobotBattery-v0'
+
+def train(env, agent, episodes):
+    for _ in range(episodes):
+        observation, _ = env.reset()
+        terminated, truncated = False, False
+        while not (terminated or truncated):
+            action = agent.get_action(observation, 'epsilon-greedy')
+            new_observation, reward, terminated, truncated, _ = env.step(action)
+            agent.update(observation, action, new_observation, reward, terminated)
+            observation = new_observation
+
+def play(env, agent):
+    observation, _ = env.reset()
+    terminated, truncated = False, False
+    while not (terminated or truncated):
+        action = agent.get_action(observation, 'greedy')
+        observation, _, terminated, truncated, _ = env.step(action)
+        env.render()
+
+if __name__ == "__main__":
+
+    env = gym.make(ENVIRONMENT)
+    agent = QLearning(env.observation_space.n, env.action_space.n, alpha=0.1, gamma=0.9, epsilon=0.1)
+
+    train(env, agent, episodes=2)
+    agent.render()
+
+    env = gym.make(ENVIRONMENT, render_mode="human")
+    play(env, agent)
+
+    env.close()
